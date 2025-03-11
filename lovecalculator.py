f4944b7b55a37d87442cd04b6c2783eebc9a6649
Bernadette Modi <berniemain353@gmail.com>
2025-03-11 13:12:18 +0300

    Created a basic HTML setup.

--- lovecalculator.py
+++ lovecalculator.py
@@ -4,3 +4,20 @@ import random
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
+def love_calculator():
+    if request.method == "POST":
+        name1 = request.form["name1"]
+        name2 = request.form["name2"]
+
+        love_score = random.randint(50, 100)
+
+        if love_score > 85:
+            message = "You two are a match made in heaven! ❤️"
+        elif love_score > 70:
+            message = "You have great chemistry! 💞"
+        else:
+            message = "There's potential! Keep the love growing! 💖"
+        return render_template("index.html", name1=name2, name2=name2, love_score=love_score, message=message)
+    return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=True)
